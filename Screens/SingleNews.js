5006f44853b1ab025168b1221e4029c849e189da
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-05-13 11:30:09 +0200

    add onboard images

--- Screens/SingleNews.js
+++ Screens/SingleNews.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ScrollView, View, Text, StyleSheet, Image } from 'react-native'
+import AppStatusBar from '../Components/AppStatusBar ';
 
 export default class SingleNews extends React.Component {
     state = {
@@ -8,7 +9,8 @@ export default class SingleNews extends React.Component {
     render() {
         return (
             <ScrollView>
-                <View style={style.mainContainer} >
+                <AppStatusBar />
+                <View View style={style.mainContainer} >
                     <Image
                         style={style.image}
                         source={{ uri: 'https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg' }}
@@ -16,7 +18,7 @@ export default class SingleNews extends React.Component {
                     <Text style={style.title}>What is Lorem Ipsum?</Text>
                     <Text style={style.description}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
                 </View>
-            </ScrollView>
+            </ScrollView >
         )
     }
 }
